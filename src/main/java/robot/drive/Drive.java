b7fc952971d91fe5f41e24e39ab12ba20f78ae6c
johnnyz27 <johnnyz27@nycstudents.net>
2024-12-09 19:38:58 -0500

    Update Drive.java

--- src/main/java/robot/drive/Drive.java
+++ src/main/java/robot/drive/Drive.java
@@ -12,6 +12,11 @@ public class Drive extends SubsystemBase {
     private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER, MotorType.kBrushless);
     private final CANSparkMax rightFollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
 
+
+     private final RelativeEncoder leftEncoder = leftLeader.getEncoder();
+     private final RelativeEncoder rightEncoder = rightLeader.getEncoder();
+
+	
     public Drive() {
       for (CANSparkMax spark : List.of(leftLeader, leftFollower, rightLeader, rightFollower)) {
 	    spark.restoreFactoryDefaults();
